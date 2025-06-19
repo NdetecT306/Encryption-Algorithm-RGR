c1b41b60546a01c7593449edbcc60060df9b1ecc
NdetecT306 <nstasya777@mail.ru>
2025-06-19 10:32:28 +0700

    Create SecTabPer.h

---
+++ SecTabPer.h
@@ -0,0 +1,42 @@
+#ifndef SEC_TAB_PER_H
+#define SEC_TAB_PER_H
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <random>
+#include <algorithm>
+#include <cmath>
+#include <locale>
+#include <codecvt>
+#include <stdexcept>
+#include <cstdint>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <fstream>
+#include <sstream>
+#include <locale>
+#include <codecvt>
+#include <cmath>
+#include <cstdint>
+#include <filesystem>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int SecTab();
+void encConsole();
+void decConsole();
+void encFile();
+void decFile();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
